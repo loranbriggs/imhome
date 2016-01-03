0a03d44213b913c3a909fcd3104ec752d7fdaa23
Loran Briggs <loranbriggs@gmail.com>
2016-01-03 00:15:26 -0800

    pass ip address to template

--- imhome.go
+++ imhome.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "html/template"
     "net/http"
     "strings"
 )
@@ -11,11 +12,26 @@ var users = map[string]string{}
 func handler(w http.ResponseWriter, r *http.Request) {
     ip := strings.Split(r.RemoteAddr, ":")[0]
     users[ip] = ip
-    fmt.Fprint(w, users)
+
+    data := struct {
+        User string
+        Users map[string]string
+    } {
+        ip,
+        users,
+    }
+
+    t, _ := template.ParseFiles("views/home.html")
+    t.Execute(w, data)
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/new", newHandler)
     fmt.Println("listing at http://localhost:8080/")
     http.ListenAndServe(":8080", nil)
 }
